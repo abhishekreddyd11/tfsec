655c921c7d4e879db435df7c1ef696b1164e5679
abhishekreddyd11 <157250867+abhishekreddyd11@users.noreply.github.com>
2024-11-22 19:47:54 -0800

    Update abgalaxy.tf

--- abgalaxy.tf
+++ abgalaxy.tf
@@ -1,57 +1,10 @@
+# S3 bucket with public ACL
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-unsecure-bucket"
-  acl    = "private" # Updated ACL to private
+  bucket = "example-vulnerable-bucket" # Bucket name
+  acl    = "public-read"              # Publicly accessible bucket
 
   tags = {
-    Name        = "example-bucket"
+    Name        = "example-vulnerable-bucket"
     Environment = "Test"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.example_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.example.arn
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.example_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_security_group" "example" {
-  ingress {
-    cidr_blocks = ["192.168.1.0/24"] # Restrict ingress to specific subnet
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
-
-  egress {
-    cidr_blocks = ["192.168.1.0/24"] # Restrict egress
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-  }
-}
-
-resource "aws_kms_key" "example" {
-  description             = "Example KMS key"
-  deletion_window_in_days = 10
 }
